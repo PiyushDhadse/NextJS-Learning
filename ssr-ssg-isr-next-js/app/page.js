0dda209447b792165052b500ddc7a9609573454a
Piyush Dhadse <dhadsepiyush54@gmail.com>
2025-12-07 14:54:04 +0530

    Understanding the basics of SSR, SSG, and ISR in Next.js

---
+++ ssr-ssg-isr-next-js/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className="container">
+        This is the home page
+      </div>
+    </div>
+  )
+}
+
+export default page
